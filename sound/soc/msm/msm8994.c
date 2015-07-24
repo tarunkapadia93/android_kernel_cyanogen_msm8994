4bd06ab1cfcbc7bbcba701418a4f9cca528e206a
chewei_liang <chewei_liang@compal.com>
2015-07-24 19:08:09 +0800

    ASoC: msm: msm8994: tune mbhc for pm9x
    Fixes issue where DUT is slow to respond to pressing
    volume -/+ during music playback
    
    Change-Id: Ia5e2eb429605c25e8361138854e358a4107d761f

--- sound/soc/msm/msm8994.c
+++ sound/soc/msm/msm8994.c
@@ -2698,6 +2698,7 @@ static void *def_codec_mbhc_cal(void)
 	btn_low = wcd9xxx_mbhc_cal_btn_det_mp(btn_cfg, MBHC_BTN_DET_V_BTN_LOW);
 	btn_high = wcd9xxx_mbhc_cal_btn_det_mp(btn_cfg,
 					       MBHC_BTN_DET_V_BTN_HIGH);
+#ifndef CONFIG_MACH_PM9X
 	btn_low[0] = -50;
 	btn_high[0] = 20;
 	btn_low[1] = 21;
@@ -2714,6 +2715,24 @@ static void *def_codec_mbhc_cal(void)
 	btn_high[6] = 269;
 	btn_low[7] = 270;
 	btn_high[7] = 500;
+#else
+	btn_low[0] = -50;
+	btn_high[0] = 80;
+	btn_low[1] = 81;
+	btn_high[1] = 82;
+	btn_low[2] = 83;
+	btn_high[2] = 104;
+	btn_low[3] = 105;
+	btn_high[3] = 148;
+	btn_low[4] = 149;
+	btn_high[4] = 189;
+	btn_low[5] = 190;
+	btn_high[5] = 245;
+	btn_low[6] = 246;
+	btn_high[6] = 269;
+	btn_low[7] = 270;
+	btn_high[7] = 530;
+#endif
 	n_ready = wcd9xxx_mbhc_cal_btn_det_mp(btn_cfg, MBHC_BTN_DET_N_READY);
 	n_ready[0] = 80;
 	n_ready[1] = 68;
